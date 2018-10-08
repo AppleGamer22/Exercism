ab6ed0c3b361345592376af3daefb2389dff8d14
Omri Bornstein <applegamer22@icloud.com>
2018-10-08 12:59:36 +1030

    etl

--- swift/etl/Sources/Etl.swift
+++ swift/etl/Sources/Etl.swift
@@ -1,1 +1,12 @@
 //Solution goes in Sources
+class ETL {
+	static func transform(_ old: [Int: [String]]) -> [String: Int] {
+		var output = [String: Int]()
+		for items in old {
+			for item in items.value {
+				output[item.lowercased()] = items.key
+			}
+		}
+		return output
+	}
+}
